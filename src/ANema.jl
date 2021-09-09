70a6b5949699bb82603d0a83de57a6cee42137a7
andLaing <a.b.r.laing@gmail.com>
2021-09-09 18:39:31 +0200

    Update ANema module definition

--- src/ANema.jl
+++ src/ANema.jl
@@ -1,5 +1,11 @@
 module ANema
+include("evtfn.jl")
+include("evtdf.jl")
+include("dftolor.jl")
+include("nema4.jl")
 
-# Write your package code here.
-
+export setunits, write_lors_hdf5, radial_correcction, 
+       deltatime, cdoi, ctsr, crt, dftolor
+export readdf, writemdf, selectinterval
+export distance_lor_to_point, sinogramdf, ztsinogram, thetasgrm, zsgrm 
 end
